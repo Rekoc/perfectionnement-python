abb9ff4d651c7cb64af844d55e701b598fc75725
Rekoc <rekoc13@gmail.com>
2022-08-03 23:45:28 +0200

    Add exercice 6

---
+++ exercice6/q1.py
@@ -0,0 +1,11 @@
+import re
+from pathlib import Path
+
+
+"""
+Fake logs, generated on https://www.logs.to/
+"""
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+PATH_TO_LOG_FILE = f"{BASE_DIR}/statics/20220803234120.cef"
+
---
+++ exos6.py
@@ -0,0 +1,8 @@
+
+
+def main():
+    print("\n# Question 1 :")
+
+
+if __name__ == '__main__':
+    main()
